eee34953928104583f4f5ce0528fddaf31927ea6
Gayuh <gayuhhsuh@gmail.com>
2025-03-19 20:57:04 +0700

    Jobsheet5-Tugas Praktikum 1

--- Minggu 5/Jobsheet 5/resources/views/kategori/index.blade.php
+++ Minggu 5/Jobsheet 5/resources/views/kategori/index.blade.php
@@ -5,6 +5,12 @@
 @section('content_header_title', 'Home')
 @section('content_header_subtitle', 'Kategori')
 
+@section('content_header')
+    <div class="d-flex justify-content-between align-items-center">
+        <h1 class="text-muted">Manage Kategori</h1>
+        <a href="{{ url('/kategori/create') }}" class="btn btn-primary">Add Kategori</a>
+    </div>
+@endsection
 @section('content')
     <div class="container">
         <div class="card">
