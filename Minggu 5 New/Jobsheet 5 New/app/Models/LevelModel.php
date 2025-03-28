7e7f6c30a1923ff892f4b347f887dae5c63407b3
Gayuh <gayuhhsuh@gmail.com>
2025-03-28 21:34:39 +0700

    Jobsheet5-Tugas Praktikum

--- Minggu 5 New/Jobsheet 5 New/app/Models/LevelModel.php
+++ Minggu 5 New/Jobsheet 5 New/app/Models/LevelModel.php
@@ -9,5 +9,6 @@ class LevelModel extends Model
 {
     use HasFactory;
     protected $table = 'm_level';
-    protected $primaryKey = 'id_level';
+    protected $primaryKey = 'level_id';
+    protected $fillable = ['level_id', 'level_kode', 'level_nama'];
 }
