abdb8106efeb71a28717c8629cb27de5ecc688fe
Gayuh <gayuhhsuh@gmail.com>
2025-04-09 19:19:32 +0700

    Jobsheet7-2. Implementasi Authorizaton di Laravel dengan Middleware Praktikum 2

--- Minggu_7/Jobsheet7/routes/web.php
+++ Minggu_7/Jobsheet7/routes/web.php
@@ -41,7 +41,9 @@ Route::middleware(['auth'])->group(function () { // artinya semua route di dalam
         Route::delete('/{id}', [UserController::class, 'destroy']); // menghapus data user
     });
 
-    Route::group(['prefix' => 'level'], function () {
+    //level
+    // Route::group(['prefix' => 'level'], function () {
+    Route::middleware(['authorize:ADM'])->group(function () {
         Route::get('/', [LevelController::class, 'index']);
         Route::post('/list', [LevelController::class, 'list']);
         Route::get('/create', [LevelController::class, 'create']);
