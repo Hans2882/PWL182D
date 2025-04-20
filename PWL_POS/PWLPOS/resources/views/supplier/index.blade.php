88199beb88a51fd1c18eeb28f42581a1b5ad11a6
Gayuh <gayuhhsuh@gmail.com>
2025-04-20 22:45:24 +0700

    PWL POS

---
+++ PWL_POS/PWLPOS/resources/views/supplier/index.blade.php
@@ -0,0 +1,90 @@
+@extends('layouts.template')
+
+@section('content')
+    <div class="card card-outline card-primary">
+        <div class="card-header">
+            <h3 class="card-title">{{ $page->title }}</h3>
+            <div class="card-tools">
+                <button onclick="modalAction('{{ url('/supplier/import') }}')" class="btn btn-info">Import Supplier</button>
+
+                <a href="{{ url('/supplier/export_excel') }}" class="btn btn-primary"><i class="fa fa-fileexcel"></i>
+                    Export Supplier</a>
+
+                <a href="{{ url('/supplier/export_pdf') }}" class="btn btn-warning"><i class="fa fa-filepdf"></i>
+                    Export Supplier PDF</a>
+
+                <a href="{{ url('/supplier/create') }}" class="btn btn-primary">Tambah Data (Ajax)</a>
+            </div>
+        </div>
+        <div class="card-body">
+            @if (session('success'))
+                <div class="alert alert-success">{{ session('success') }}</div>
+            @endif
+            @if (session('error'))
+                <div class="alert alert-danger">{{ session('error') }}</div>
+            @endif
+
+            <table class="table table-bordered table-striped table-hover table-sm" id="table_supplier">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Kode Supplier</th>
+                        <th>Nama Supplier</th>
+                        <th>Alamat Supplier</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
+    <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog"
+        databackdrop="static"data-keyboard="false" data-width="75%" aria-hidden="true"></div>
+@endsection
+
+@push('js')
+    <script>
+        function modalAction(url = '') {
+            $('#myModal').load(url, function() {
+                $('#myModal').modal('show');
+            });
+        }
+
+        var dataUser;
+        $(document).ready(function() {
+            dataUser = $('#table_supplier').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: {
+                    url: "{{ url('supplier/list') }}",
+                    type: "POST",
+                    dataType: "json"
+                },
+                columns: [{
+                        data: "DT_RowIndex",
+                        className: "text-center",
+                        width: "5%",
+                        orderable: false,
+                        searchable: false
+                    },
+                    {
+                        data: "supplier_kode",
+                        orderable: true
+                    },
+                    {
+                        data: "supplier_nama",
+                        orderable: true
+                    },
+                    {
+                        data: "supplier_alamat",
+                        orderable: true
+                    },
+                    {
+                        data: "aksi",
+                        orderable: false,
+                        searchable: false
+                    }
+                ]
+            });
+        });
+    </script>
+@endpush
